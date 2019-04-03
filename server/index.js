8b629b8a4df0ada5a77ff4ef6a17d104d6fd3934
Hano <hinsul3113@gmail.com>
2019-04-02 22:27:05 -0700

    added index route and nodemon

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ const express    = require('express'),
       http       = require('http'),
       bodyParser = require('body-parser'),
       morgan     = require('morgan'),
+      router     = require('./routes/router'),
       app        = express();
 
 // App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
 
+router(app);
 
 // Server Setup
 const PORT = process.env.PORT || 3010;
