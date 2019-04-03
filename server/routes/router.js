fff0c4971930b54a48148c5345315dd61d884e31
Hano <hinsul3113@gmail.com>
2019-04-03 16:33:42 -0700

    Initial commit for client

--- server/routes/router.js
+++ server/routes/router.js
@@ -6,6 +6,8 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = app => {
+  // for any other route to provide authentication, just add requireAuth
+  // requireAuth prevents unauthorized users from accessing any route that we add
   app.get('/', requireAuth, function(req, res) {
     res.send({ test: 'worked! :)' });
   });
