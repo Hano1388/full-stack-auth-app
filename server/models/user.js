ec88d467c77bfdef000357f315addb5562b3c541
Hano <hinsul3113@gmail.com>
2019-04-03 01:36:33 -0700

    encypted passwords using bcrypt-nodejs

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,33 @@
 const mongoose = require('mongoose'),
-    { Schema } = mongoose;
+    { Schema } = mongoose,
+    bcrypt     = require('bcrypt-nodejs');
 
 // Define the user model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
-})
+});
+
+// On Save Hook, encrypt password
+// Before saving a model, run this function
+userSchema.pre('save', function(next) {
+  // get access to the user model
+  const user = this;
+
+  // generate a salt then run callback
+  bcrypt.genSalt(10, function(err, salt) {
+    if(err) { return next(err) }
+
+    // hash (encrypt) password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if(err) { return next(err) }
+
+      // overwrite plain text password with hash
+      user.password = hash;
+      next();
+    });
+  });
+});
 // Create User Class
 const UserClass = mongoose.model('user', userSchema);
 // Export it
