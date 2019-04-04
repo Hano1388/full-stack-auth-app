c90b01dca3f3ecdfc74f4c6713c5c62653ba5f4e
Hano <hinsul3113@gmail.com>
2019-04-04 02:32:09 -0700

    styled header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,23 +2,38 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = (props) => {
+  const [homeClass, setHomeClass] = useState('active item');
+  const [signinClass, setSinginClass] = useState('item');
+  const [signupClass, setSignupClass] = useState('item');
+  const [signoutClass, setSignoutClass] = useState('item');
+
+  const changeClassNames = (className) => {
+    className === 'home'? setHomeClass('active item') : setHomeClass('item');
+    className === 'signin'? setSinginClass('active item') : setSinginClass('item');
+    className === 'signup'? setSignupClass('active item') : setSignupClass('item');
+    className === 'signout'? setSignoutClass('active item') : setSignoutClass('item');
+  }
   return(
     <div className="ui container">
-      <div className="ui four item menu">
+      <div className="ui four item pointing menu">
         <Link
-          className="item"
+          className={homeClass}
+          onClick={() => changeClassNames('home') }
           to="/home"
         >Redux auth</Link>
         <Link
-          className="item"
+          className={signinClass}
+          onClick={() => changeClassNames('signin') }
           to="/signin"
         >Sign In</Link>
         <Link
-          className="item"
+          className={signupClass}
+          onClick={() => changeClassNames('signup') }
           to="/signup"
         >Sign Up</Link>
         <Link
-          className="item"
+          className={signoutClass}
+          onClick={() => changeClassNames('signout') }
           to="/signout"
         >Sign Out</Link>
       </div>
